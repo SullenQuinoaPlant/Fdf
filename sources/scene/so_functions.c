5adfb4d8bb85c65e67393bb2870dc6505b7f65b8
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-29 23:12:42 +0200

    i

--- sources/scene/so_functions.c
+++ sources/scene/so_functions.c
@@ -1,48 +1,5 @@
 #include "scene.h"
 
-int						get_nxt_obj(
-	t_s_s *scene,
-	t_tag *ret,
-	t_s_o **retaddr)
-{
-	t_s_so *const	p = scene->os;
-	size_t const	sz = TAS * T_S;
-	t_s_ft			*ftgs;
-	int				r;
-
-	while (!(ftgs = (t_s_fsp*)p->nxt->content))
-		if ((r = TAC - scene->ar_allocs < sz ? MEM_CAP : 0) ||
-			(r = add_star(add_obj_ar, sz, s)) != SUCCESS)
-			return (r);
-	if ((*ret = ftgs->free++) == fp->last)
-	{
-		ft_lstdelhead(&p->nxt);
-		scene->nxt_allocs--;
-	}
-	if (retaddr)
-		*retaddr = &(scene->os.ar[*ret >> TPS])[*ret & TPM];
-	return (SUCCESS);
-}
-
-int						new_scene_obj(
-	t_s_s *scene,
-	t_s_o **ret,
-	t_tag *ret_tag)
-{
-	t_tag	tag;
-	t_s_o	*obj;
-	int		r;
-
-	*ret = 0;
-	if ((r = get_nxt_obj(scene, &tag, &obj)) != SUCCESS ||
-		(r = get_nxt_uspsv(scene, &tag)) != SUCCESS)
-		return (r);
-	ft_bzero(obj, sizeof(t_s_o));
-	obj->refct = 1;
-	if (ret_tag)
-		*ret_tag = tag;
-	return (r);
-}
 int						nxt_fresh_obj(
 	t_s_s *scene,
 	t_s_o **ret,
@@ -71,7 +28,7 @@ int						nxt_active_obj(
 	int		r;
 
 	if ((r = nxt_fresh_obj(s, ret, ret_tag)) == SUCCESS &&
-		(r = ring_expand(sizeof(t_s_sao), 0, &s->ao) == RING_SUCCESS))
+		(r = ring_expand(sizeof(t_s_ao), 0, &s->ao) == RING_SUCCESS))
 	{
 		o->refs = 1;
 		s->ao->flags |= SHOW_O;
