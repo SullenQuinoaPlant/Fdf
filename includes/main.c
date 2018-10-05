bea8a8c6999a5c9f5d142bd7703402ee8e0ef4b1
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-05 03:01:36 +0200

    i

---
+++ includes/main.c
@@ -0,0 +1,9 @@
+#include <math.h>
+#include <stdio.h>
+
+int		main()
+{
+	printf("%a\n", sqrt(2));
+	printf("%a\n", sqrt(3));
+	printf("%a\n", sqrt(6));
+}
---
+++ sources/parse/parse.c
@@ -0,0 +1,138 @@
+#include "parsed.h"
+#include "parse.h"
+
+#define BUF_CT 256
+#define BUF_SZ BUF_CT * sizeof(t_s_cxyd)
+
+int							new_buf(
+	t_list **prv,
+	size_t *count,
+	t_s_cxyd **p)
+{
+	t_list		*new;
+
+
+	if ((new = ft_lstnew(0, 0)) &&
+		(*p = malloc(BUF_SZ))
+	{
+		new->content = ar;
+		new->content_size = BUF_SZ;
+		ft_lstadd(prv, new);
+		*count = BUF_CT;
+	}
+	else if (new)
+		free(new);
+	return (p_ar ? SUCCESS : SYS_ERR);
+}
+
+#define NOT_DONE 1
+static int					parse_sncnl_like_really
+	char const **str,
+	t_s_cxyd **p)
+{
+	char const	*s = *str;
+	t_argb		col;
+
+	col = 0;
+	if (!*s)
+		return (SUCCESS);
+	if (*s++ != ' ' || ft_atoierr(s, &z, &s) ||
+		(*s == ',' && ft_axtoierr(++s, &col, &s))
+		return (BAD_INFILE);
+	*((*p)++) = (t_s_cxyd){z, col};
+	return (NOT_DONE);
+}
+
+#define DIM_L 0
+#define DIM_C 1
+#define E_CT 0
+#define E_LEFT 1
+int							parse_cdgfxyrz_sncnl(
+	int fd,
+	size_t *dims,
+	t_list **bs)
+{
+	char		*l[2];
+	size_t		l_ct
+	size_t		e_ct[2];
+	t_s_cxyd	*p;
+	int			r;
+
+	*bs = 0;
+	l_ct = 0;
+	ft_memset(e_ct, 0, 2);
+	while ((r = get_next_line(fd, &l[0]) > 0 ? SUCCESS : SYS_ERR) == SUCCESS) 
+	{
+		l[1] = l[0];
+		l_ct++;
+		while ((e_ct || (r = new_buff(bs, &e_ct, &p)) == SUCCESS) &&
+			(r = parse_sncnl_like_really(&l[1], &p) == SUCCESS))
+			e_ct--;
+		free(l[0]);
+		if (r != SUCCESS)
+			break ;
+	}
+	dims[DIM_L] = l_ct;
+	dims[DIM_C] = e_ct[E_CT] * BUF_CT - e_ct[E_LEFT];
+	return (r);
+}
+
+int							open_sncnl_file(
+	char const *file,
+	int *ret_fd)
+{
+	int		r;
+
+	if ((*ret_fd = open(file, O_RDONLY)) < 0)
+		return (SYS_ERR);
+	return (SUCCESS);
+}
+
+#define USE 0
+#define SAVE 1
+int							fill_tscdgfxyrz(
+	size_t *dims,
+	t_list *bs,
+	t_s_cdgfxyrz *ret)
+{
+	size_t		ct_all;
+	size_t		sz;
+	t_s_cxyd	*ar[2];
+
+	ct_all = dims[DIM_L] * dims[DIM_C];
+	if (!(ar[USE] = malloc(sizeof(t_s_cxyd) * ct_all)))
+		return (SYS_ERR);
+	ar[SAVE] = ar[USE];
+	ft_memcpy(*ar, bs->content, (sz = (ct_all % BUF_CT) * sizeof(t_s_cxyd)));
+	ar[USE] += sz;
+	sz = BUF_CT * sizeof(t_s_cxyd);
+	while (ct_all >= BUF_CT)
+	{
+		bs = bs->next;
+		ft_memcpy(*ar, bs->content, sz);
+		*ar += sz;
+	}
+	ret->x_sz = dims[DIM_C];
+	ret->y_sz = dims[DIM_L];
+	ft_memcpy(ret->at, &(double[3]){0, 0, 0};
+	return (SUCCESS);
+}
+
+int							get_tscdgfxyrz(
+	char const *file,
+	t_s_cdgfxyrz *ret)
+{
+	int				fd;
+	size_t			dims[2];
+	t_list			*bs;
+	t_s_cdgfxyrz	*p)
+
+	if ((r = open_sncnl_file(file, &fd)) == SUCCESS &&
+		(r = (parse_cdgfxyrz_sncnl(fd, dims, &bs))) == SUCCESS &&
+		(p = malloc(sizeof(t_s_cdgfxyrz))))
+		r = fill_tscdgfxyrz(dims, bs, ret);
+	if (fd >= 0)
+		close(fd);
+	ft_lstdel(&bs, 0);
+	return (r)
+}
