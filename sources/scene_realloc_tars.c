255f9aa963395dc150cdcb1003598de917bdccdd
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-10 12:53:24 +0200

    i

---
+++ sources/scene_realloc_tars.c
@@ -0,0 +1,14 @@
+#include "scene_typedefs.h"
+
+#include "scene_typedefs.h"
+
+#include "scene_typedefs.h"
+
+#include "scene_typedefs.h"
+
+int						realloc_tars(
+	t_s_s *s)
+{
+	(void)s;
+	return (SUCCESS);
+}
