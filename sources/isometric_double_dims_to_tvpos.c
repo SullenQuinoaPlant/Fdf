ee289734c43787ab37fd4fb62813a322ddcd7ce3
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-13 03:03:14 +0200

    i

--- sources/isometric_double_dims_to_tvpos.c
+++ sources/isometric_double_dims_to_tvpos.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   doubles_to_tvpos.c                                 :+:      :+:    :+:   */
+/*   isometric_double_dims_to_tvpos.c                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/12 03:26:50 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/10/13 02:56:44 by nmauvari         ###   ########.fr       */
+/*   Created: 2018/10/13 02:57:30 by nmauvari          #+#    #+#             */
+/*   Updated: 2018/10/13 02:58:59 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scene_typedefs.h"
 
-void							doubles_to_tvpos(
+void							iso_dbl_dims_to_tvpos(
 	t_s_sv *v,
-	double coords[DIMS],
+	double pos[DIMS],
 	t_vpos *ret)
 {
-	int		i;
-
-	i = -1;
-	while (++i < V_DIMS)
-		ret[i] = (t_vuint)coords[i];
+	wh
 }
