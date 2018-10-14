cab7815c604ccd2462449e685219036fc272b64c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-14 07:54:04 +0200

    i

--- sources/mirror_tsta.c
+++ sources/mirror_tsta.c
@@ -2,19 +2,21 @@
 
 int							mirror_tsta_allocs(
 	t_s_s *s,
-	t_s_ta const *in,
-	t_s_ta *out)
+	t_s_ta const *,
+	t_s_ta *reflect)
 {
+	size_t const	oe_sz = out->e_sz
 	size_t			ar_sz;
 	size_t			sz;
+	void			**ar;
 	int				r;
 
 	ar_sz = in->ar_sz;
 	sz = out->e_sz * TAS * ar_sz;
 	if(!(r = TAC - s->tar_allocs < sz ? MEM_CAP : 0) &&
-		(out->ar = malloc(ar_sz * sizeof(void*))))
+		(ar = malloc(ar_sz * sizeof(void*))))
 	{
-		while (ar_sz--)
+		while (ar_sz-- && (r = tar_alloc(s, oe_sz, ar)) == SUCCESS)
 		ft_memcpy(ar, *p_ar, (sz = prv_sz * sizeof(void*)));
 		ft_cleanfree(*p_ar, sz);
 		*p_ar = ar;
