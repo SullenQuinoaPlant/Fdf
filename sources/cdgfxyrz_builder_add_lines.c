994016b3b6fe8c2c434bbac34748eb4d1720db32
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-18 10:59:12 +0200

    i

--- sources/cdgfxyrz_builder_add_lines.c
+++ sources/cdgfxyrz_builder_add_lines.c
@@ -6,7 +6,7 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/08 04:53:58 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/10/18 09:12:55 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/10/18 10:58:30 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,37 @@
 #include "scene.h"
 #include "parse.h"
 
-#define I 0
-#define J 1
+static int				add_y_lines(
+	t_s_cdgfxyrz *p,
+	t_tag *tags,
+	t_s_s *s,
+	t_list **p_list)
+{
+	t_tag (*const	tar)[p->y_sz] = (t_tag(*)[p->y_sz])tags;
+	size_t			i;
+	size_t			j;
+	t_s_l			l;
+	t_s_l			*p_e;
+
+	i = -1;
+	while (++i < p->x_sz)
+	{
+		j = 0;
+		l = (t_s_l){1, {0, tar[i][0]}, {0, p->ar[i * p->y_sz].col}};
+		while (++j < p->y_sz)
+		{
+			if (get_nxt_se(e_l, s, p_l->content, (void**)&p_e) != SUCCESS ||
+				ft_lstaddnew(p_list, &(t_tag)0, sizeof(t_tag)))
+				return (SYS_ERR);
+			l.ends[0] = l.ends[1];
+			l.argb[0] = l.argb[1];
+			l.ends[1] = tar[i][j];
+			l.argb[1] = p->ar[p->y_sz * i + j].col;
+			*p_e = l;
+		}
+	}
+	return (SUCCESS);
+}
 
 static int				add_x_lines(
 	t_s_cdgfxyrz *p,
@@ -23,63 +52,29 @@ static int				add_x_lines(
 	t_s_s *s,
 	t_list **p_list)
 {
-	size_t	i;
-	size_t	j;
-	t_s_l	l;
-	t_s_l	*p_e;
-	t_list	*p_l;
+	t_tag (*const	tar)[p->y_sz] = (t_tag(*)[p->y_sz])tags;
+	size_t			i;
+	size_t			j;
+	t_s_l			l;
+	t_s_l			*p_e;
 
-	p_l = *p_list;
 	i = -1;
-	while (++i < p->x_sz && !(j = 0))
+	while (++i < p->y_sz)
 	{
-		l = (t_s_l){1, {tags[i * p->y_sz], 0}, {p->ar[i * p->y_sz].col, 0}};
-		while (++j < p->y_sz)
-		{
-			if ((get_nxt_se(e_l, s, p_l->content, (void**)&p_e)) != SUCCESS)
-				return (SYS_ERR);
-			l.ends[0] = l.ends[1];
-			l.argb[0] = l.argb[1];
-			l.ends[1] = tags[i * p->y_sz + j];
-			l.argb[1] = p->ar[i * p->y_sz + j].col;
-			*p_e = l;
-			p_l = p_l->next;
-		}
-	}
-	*p_list = p_l;
-	return (SUCCESS);
-}
-
-static int				add_y_lines(
-	t_s_cdgfxyrz *p,
-	t_tag *tags,
-	t_s_s *s,
-	t_list **p_list)
-{
-	size_t	i;
-	size_t	j;
-	t_s_l	l;
-	t_s_l	*p_e;
-	t_list	*p_l;
-
-	p_l = *p_list;
-	i = -1;
-	while (++i < p->y_sz && !(j = 0))
-	{
-		l = (t_s_l){1, {tags[i], 0}, {p->ar[i].col, 0}};
+		j = 0;
+		l = (t_s_l){1, {0, tags[i]}, {0, p->ar[i].col}};
 		while (++j < p->x_sz)
 		{
-			if ((get_nxt_se(e_slna, s, p_l->content, (void**)&p_e)) != SUCCESS)
+			if (get_nxt_se(e_l, s, p_l->content, (void**)&p_e) != SUCCESS ||
+				ft_lstaddnew(p_list, &(t_tag)0, sizeof(t_tag)))
 				return (SYS_ERR);
 			l.ends[0] = l.ends[1];
 			l.argb[0] = l.argb[1];
-			l.ends[1] = tags[i + p->y_sz * j];
-			l.argb[1] = p->ar[i + p->y_sz * j].col;
+			l.ends[1] = tar[j][i];
+			l.argb[1] = p->ar[p->y_sz * j + i].col;
 			*p_e = l;
-			p_l = p_l->next;
 		}
 	}
-	*p_list = p_l;
 	return (SUCCESS);
 }
 
@@ -89,11 +84,8 @@ int						cdgfxyrz_add_lines(
 	t_s_s *s,
 	t_s_o *o)
 {
-	t_list	*p_l;
-
-	p_l = o->e[e_ol];
-	if (add_x_lines(p, tags, s, &p_l) == SUCCESS &&
-		add_y_lines(p, tags, s, &p_l) == SUCCESS)
+	if (add_x_lines(p, tags, s, &o->e[e_ol]) == SUCCESS &&
+		add_y_lines(p, tags, s, &o->e[e_ol]) == SUCCESS)
 		return (SUCCESS);
 	return (SYS_ERR);
 }
