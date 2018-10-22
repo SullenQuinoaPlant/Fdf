baa103c14c82aa858445256fcc173ae2cd52b1c0
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-22 10:16:46 +0200

    i

--- sources/point_projection.c
+++ sources/point_projection.c
@@ -15,3 +15,17 @@ void						point_projection(
 	txyz_to_ttpr(pp->xyz, pp->tpr);
 	v->e[e_p].tick = v->ct->tick;
 }
+
+void						point_projection_iso_persist(
+	t_s_sv *v,
+	void *scene_point,
+	t_s_pp const *const *points,
+	void *view_point)
+{
+	t_s_pp *const	pp = (t_s_pp*)view_point;
+	t_vpos			hw;
+
+	point_projection(v, scene_point, points, view_point);
+	if (iso_dbl_dims_to_tvpos(v, pp->xyz, hw) == SUCCESS)
+		mlx_pixel_put(v->s->mlx_ptr, v->win_ptr, hw[V_W], hw[V_H], COL_WHITE); 
+}
